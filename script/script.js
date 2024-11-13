e022e01bda02c7561dbe79b360d6d8032ebec764
Guilherme Ramon <guilhermeramon996@gmail.com>
2024-11-13 11:09:05 -0300

    script.js

--- script/script.js
+++ script/script.js
@@ -9,14 +9,6 @@ const cartCounter = document.getElementById("cart-count");
 const addressInput = document.getElementById("address");
 const addressWarn = document.getElementById("address-warn");
 
-// Função para fechar o alerta manualmente
-  function closeAlert() {
-    document.getElementById('alert').style.display = 'none';
-  }
-
-  // Configura o alerta para desaparecer automaticamente após 3 segundos
-  setTimeout(closeAlert, 5000);
-
 let cart =[];
 
 // abrir o carrinho
